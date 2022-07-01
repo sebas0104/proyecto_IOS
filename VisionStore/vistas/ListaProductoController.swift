1aa534c3ca8593430cf715a78938ab817dbec073
Ignacio <ignacio@MacBook-Pro-de-Ignacio.local>
2022-07-01 14:28:49 -0700

    listado producto

--- VisionStore/vistas/ListaProductoController.swift
+++ VisionStore/vistas/ListaProductoController.swift
@@ -1,29 +1,61 @@
-//
-//  ListaProductoController.swift
-//  VisionStore
-//
-//  Created by sebastian aranda on 30/06/22.
-//
-
 import UIKit
+import FirebaseFirestore
 
-class ListaProductoController: UIViewController {
-
+class ListaProductoController: UIViewController,
+                               UITableViewDelegate, UITableViewDataSource{
+    
+    
+    @IBOutlet weak var tablaProductos: UITableView!
+    
+    var listaProductos = [Producto]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.listarProductos()
+        tablaProductos.dataSource = self
+        tablaProductos.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        listaProductos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celda = tablaProductos.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let producto = listaProductos[indexPath.row]
+        
+        celda.textLabel?.text = producto.toString()
+        
+        return celda
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func listarProductos(){
+        
+        let db = Firestore.firestore()
+        db.collection("Producto").getDocuments{
+            (querySnapshot, err) in
+            self.listaProductos.removeAll()
+            if let err = err{
+                print("Error al traer los productos: \(err)")
+            }else{
+                for document in
+                        querySnapshot!.documents{
+                    let producto = Producto(IDproducto: document.data()["IDproducto"] as! Int,
+                        Nomproduc:document.data()["Nomproduc"] as! String,Descripcion: document.data()["Descripcion"] as! String, Cantidad: document.data()["Cantidad"] as! Int, Precio: document.data()["Precio"] as! Double)
+                    
+                    self.listaProductos.append(producto)
+                    self.tablaProductos.reloadData()                }
+                
+            }
+        }
+        
     }
-    */
+    
+    
 
 }
