54a1444863fc64ac4dbe422199bce4695e905ddc
sebas0104 <sebastian.am345@gmail.com>
2022-06-30 23:30:51 -0500

    Inicio proyecto

---
+++ VisionStore/modelos/Producto.swift
@@ -0,0 +1,23 @@
+import Foundation
+class Producto{
+
+var IDproducto: Int
+var Nomproduc: String
+var Descripcion: String
+var Cantidad: Int
+var Precio: Double
+
+init(IDproducto: Int,Nomproduc: String,Descripcion: String,Cantidad: Int,Precio: Double){
+    self.IDproducto = IDproducto
+    self.Nomproduc = Nomproduc
+    self.Descripcion = Descripcion
+    self.Cantidad = Cantidad
+    self.Precio = Precio
+    
+}
+
+func toString()-> String{
+    return "Producto: \(IDproducto) - \(Nomproduc) - \(Descripcion) -\(Cantidad)- S/. \(Precio)"
+}
+    
+}
